17f64407b64939870fa5b1cb0d4ae2246a00e6e5
Nikesh kumar <nikesh188insta@gmail.com>
2025-03-07 20:31:11 +0530

    update

--- Frontend/src/pages/Create_product.jsx
+++ Frontend/src/pages/Create_product.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from "react";
 import { NavLink,} from 'react-router-dom';
+import axiosInstance from '../config/axios.config';
 
 const Create_product = () => {
 
